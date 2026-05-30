ce08e199b79b3d7637de04e0fed4f9abbe6a43db
Sho Teshigahara <shoteshigahara@ShonoMacBook-Pro.local>
2026-05-30 12:36:42 +0900

    Bundle Vercel API function

---
+++ api/[...path].js
@@ -0,0 +1,1 @@
+export { default } from "../dist/vercel-api.js";
--- server/_core/vercel.ts
+++ server/_core/vercel.ts
@@ -1,7 +1,10 @@
 import type { IncomingMessage, ServerResponse } from "http";
-import { createApiApp } from "../server/_core/index";
+import { createApiApp } from "./index";
 
-const app = createApiApp();
+const app = createApiApp() as unknown as (
+  req: IncomingMessage,
+  res: ServerResponse
+) => void;
 
 export default function handler(req: IncomingMessage, res: ServerResponse) {
   return app(req, res);
